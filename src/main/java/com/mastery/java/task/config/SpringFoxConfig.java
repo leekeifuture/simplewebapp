6d19ae35c8a38cde3864f943a556cb8df787ae62
leekeifuture <opnoe09@gmail.com>
2020-04-15 13:29:41 +0300

    Added Swagger

---
+++ src/main/java/com/mastery/java/task/config/SpringFoxConfig.java
@@ -0,0 +1,43 @@
+package com.mastery.java.task.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import java.util.Collections;
+
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+@Import(BeanValidatorPluginsConfiguration.class)
+public class SpringFoxConfig {
+
+    @Bean
+    public Docket api() {
+        // could use any(), none(), regex() or ant()
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.mastery.java.task.rest"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "|My REST API|",
+                "|Some custom description of API.|",
+                "|API TOS|",
+                "|Terms of service|",
+                new Contact("|John Doe|", "|www.somesite.com|", "|someemail@company.com|"),
+                "|License of API|", "|API license URL|", Collections.emptyList());
+    }
+}
